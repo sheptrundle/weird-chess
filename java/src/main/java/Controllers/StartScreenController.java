afac00b2cd67974d5abee0bcdfaf5d61922a3f7d
sheptrundle <dvf5rd@virginia.edu>
2025-12-14 15:46:55 -0500

    Added start-screen.fxml and StartScreenController and ensured fxml is in modules

---
+++ java/src/main/java/Controllers/StartScreenController.java
@@ -0,0 +1,12 @@
+package Controllers;
+import javafx.fxml.FXML;
+
+import javafx.event.ActionEvent;
+
+public class StartScreenController {
+
+    @FXML
+    public void handleStartGame(ActionEvent event) {
+
+    }
+}
